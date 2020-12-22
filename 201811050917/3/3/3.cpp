46c5c6ddf5747aa229c881f2a6841342e65e0136
FJLEGEND <1026280076@qq.com>
2020-12-22 11:38:00 +0800

    2

--- 201811050917/3/3/3.cpp
+++ 201811050917/3/3/3.cpp
@@ -1,11 +1,43 @@
 ﻿// 3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+//某小学最近得到了一笔赞助，打算拿出其中一部分为学习成绩优秀的前5名学生发奖学金。期末，每个学生都有3门课的成绩:语文、数学、英语。
+//先按总分从高到低排序，如果两个同学总分相同，再按语文成绩从高到低排序，如果两个同学总分和语文成绩都相同，那么规定学号小的同学 排在前面，这样，每个学生的排序是唯一确定的。
+//任务：先根据输入的3门课的成绩计算总分，然后按上述规则排序，最后按排名顺序输出前五名名学生的学号和总分。注意，在前5名同学中，每个人的奖学金都不相同，因此，你必须严格按上述规则排序。
 #include<stdio.h>
 #include <iostream>
 using namespace std;
+struct grade
+{
+    int num=1;
+    int chinese=0;
+    int math=0;
+    int english=0;
+    int total_points = chinese + math + english;
+};
+void fun(grade x)
+{
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n;
+    grade student[10];
+    cout << "输入参加评选的学生人数(小于10)：";
+    cin >> n;
+    cout << endl;
+    //输入学生信息
+    for (int j = 1; j <= n; j++)
+    {
+        student[j].num = j;
+        student[j].chinese = ;
+        student[j].math = ;
+        student[j].english = ;
+    }
+    //列总成绩表
+    cout << "列出总成绩表（按学号顺序）：" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<chinese
+    }
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
