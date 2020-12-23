ac819501a8103ee47db59e93eda61482b58090ae
FJLEGEND <1026280076@qq.com>
2020-12-23 11:21:59 +0800

    3

--- 201811050917/3/3/3.cpp
+++ 201811050917/3/3/3.cpp
@@ -4,39 +4,100 @@
 //任务：先根据输入的3门课的成绩计算总分，然后按上述规则排序，最后按排名顺序输出前五名名学生的学号和总分。注意，在前5名同学中，每个人的奖学金都不相同，因此，你必须严格按上述规则排序。
 #include<stdio.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 struct grade
 {
-    int num=1;
-    int chinese=0;
-    int math=0;
-    int english=0;
-    int total_points = chinese + math + english;
+    int num;
+    int chinese;
+    int math;
+    int english;
+    int total_points;
 };
-void fun(grade x)
+void fun(grade x[],int n)
 {
-
+    int i, j;
+    grade temp;
+    //先按总分从高到低
+    for (i = 0; i < n-1 ; i++)
+    {
+        for (j = 0; j < n-1 - i; j++)
+        {
+            if (x[j].total_points < x[j + 1].total_points)
+            {
+                temp = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp;
+            }
+            else if (x[j].total_points == x[j + 1].total_points)
+            {
+                if (x[j].chinese < x[j + 1].chinese)
+                {
+                    temp = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = temp;
+                }
+                else if (x[j].chinese == x[j + 1].chinese)
+                {
+                    if (x[j].num > x[j + 1].num)
+                    {
+                        temp = x[j];
+                        x[j] = x[j + 1];
+                        x[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+    //如果两个同学总分相同，再按语文成绩从高到低排序
+   //如果两个同学总分和语文成绩都相同，那么规定学号小的同学 排在前面
 }
 int main()
 {
     int n;
+    int point[100];
     grade student[10];
     cout << "输入参加评选的学生人数(小于10)：";
     cin >> n;
     cout << endl;
     //输入学生信息
-    for (int j = 1; j <= n; j++)
+    ifstream  afile;
+    afile.open("data.txt",ios::in);
+    if (!afile)
     {
-        student[j].num = j;
-        student[j].chinese = ;
-        student[j].math = ;
-        student[j].english = ;
+        cout << "文件不能打开" << endl;
     }
+    else
+    {
+        for (int p = 0; p < 100; p++)
+        {
+            afile >> point[p];
+        }
+    }
+    for (int j = 0; j < n; j++)
+    {
+        static int j2 = 0;
+        student[j].num = j+1;
+        student[j].chinese = point[j2];
+        student[j].math =point[j2+1] ;
+        student[j].english = point[j2+2];
+        student[j].total_points = student[j].chinese + student[j].math + student[j].english;
+        j2 += 3;
+    }
+    afile.close();
     //列总成绩表
     cout << "列出总成绩表（按学号顺序）：" << endl;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<chinese
+        cout << student[i].chinese << " " << student[i].math << " " << student[i].english << endl;
+    }
+    //排序
+    fun(student, n);
+    cout << "排名为：" << endl;
+    for (int i = 0; i <5; i++)
+    {
+        
+        cout << student[i].num << " " << student[i].total_points << endl;
     }
 }
 
